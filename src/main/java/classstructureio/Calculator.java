ad9f01c52db431fdbbb6574f7e6f5ac80e48bc64
Eszter <egyd.eszter@gmail.com>
2020-11-02 04:25:07 +0100

    Trainig-solutions

---
+++ src/main/java/classstructureio/Calculator.java
@@ -0,0 +1,17 @@
+package classstructureio;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println("Give me a number!");
+        Scanner scanner = new Scanner(System.in);
+        int firstNumber = scanner.nextInt();
+
+        System.out.println("Give me another number!");
+        int secondNumber = scanner.nextInt();
+
+        System.out.println(firstNumber + " + " + secondNumber);
+        System.out.println(firstNumber + secondNumber);
+    }
+}
