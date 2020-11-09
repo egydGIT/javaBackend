b54b5826cb96a207e72d269c9fde900595477b0b
Eszter <egyd.eszter@gmail.com>
2020-11-09 13:51:23 +0100

    ez működik.

---
+++ src/main/java/packages/main/MainProgram.java
@@ -0,0 +1,10 @@
+package packages.main;
+
+import packages.greetings.Greeter;
+
+public class MainProgram {                                 // ez működik. automatikusan importálta a classt
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
+    }
+}
