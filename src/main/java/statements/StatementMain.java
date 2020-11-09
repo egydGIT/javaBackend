4b4441e214eb8c380b13c78d189db36fcdea0888
Eszter <egyd.eszter@gmail.com>
2020-11-09 11:16:04 +0100

    Javítás 'solutions' alapján:  egyszerűbben

--- src/main/java/statements/StatementMain.java
+++ src/main/java/statements/StatementMain.java
@@ -6,12 +6,16 @@ public class StatementMain {
         int y = 11 - x;
         int z = 8;
 
-        boolean b;
-        System.out.println( b = (y < x) ? true : false);
+        boolean b = y < x;                              // Javítás
         System.out.println(b);
 
-        boolean c;
-        System.out.println( c = (b == true) || (5 < z) ? true : false);
+        System.out.println( b = (y < x) ? true : false);  // Ez végül is működik, de megbonyolítottam.
+        System.out.println(b);
+
+        boolean c = b || (5 < z);                         // Javítás
+        System.out.println(c);
+
+        System.out.println( c = (b == true) || (5 < z) ? true : false);   // Ez is működik, de ezt is megbonyolítottam.
         System.out.println(c);
 
         z ++;
