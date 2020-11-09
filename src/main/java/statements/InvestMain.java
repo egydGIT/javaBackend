9e87d42cbeb51ca2161a94e62225a080728f3a7d
Eszter <egyd.eszter@gmail.com>
2020-11-09 13:39:06 +0100

    jónak tűnik a megoldásom, de adatbekérés után leáll. kivettem a 3. scennert de akkor is leáll.

--- src/main/java/statements/InvestMain.java
+++ src/main/java/statements/InvestMain.java
@@ -6,21 +6,22 @@ public class InvestMain {
     public static void main(String[] args) {                                        // ezen még gondolkozom.
         System.out.println("Kérem adja meg a befektetett összeget: ");
         Scanner scanner = new Scanner(System.in);
-        int fund = scanner.nextInt();
+        int amount = scanner.nextInt();
 
         System.out.println("Kérem adja meg a kamatlábat: ");
         int interestRate = scanner.nextInt();
 
-        Investment investment = new Investment(fund, interestRate);
+        Investment investment = new Investment(amount, interestRate);
 
         System.out.println("Tőke: " + investment.getFund());
 
-        System.out.println("Kérem adja meg, a lekötés napjainak számát: ");
-        int days = scanner.nextInt();
-        System.out.println("Hozam: " + days + "nap után: " + investment.getYield(days));
+        // System.out.println("Kérem adja meg, a lekötés napjainak számát: ");
+        // int days = scanner.nextInt();
+        // System.out.println("Hozam: " + days + "nap után: " + investment.getYield(days));
+        System.out.println("Hozam 90 nap után: " + investment.getYield(90));
 
-        System.out.println("Kivett összeg " + days + "nap után: " + investment.close(days));
-
+        // System.out.println("Kivett összeg " + days + "nap után: " + investment.close(days));
+        System.out.println("Kivett összeg 90 nap után: " + investment.close(90));
 
     }
 }
