b95101b51952a5c9cf483cbe89d63b9b3b40643f
Eszter <egyd.eszter@gmail.com>
2020-11-09 13:59:38 +0100

    solution megoldás is beleírva

--- src/main/java/packages/greetings/Greeter.java
+++ src/main/java/packages/greetings/Greeter.java
@@ -1,7 +1,14 @@
 package packages.greetings;
 
 public class Greeter {
-    public String sayHello() {
+/*
+    public String sayHello() {                  // ezt írtam én
         return "Hello World!";
     }
+*/
+
+    public void sayHello() {                   // mert a sout meghívja a toString() metódust... :)
+        System.out.println("Hello World!");
+    }
+
 }
--- src/main/java/packages/main/MainProgram.java
+++ src/main/java/packages/main/MainProgram.java
@@ -4,7 +4,9 @@ import packages.greetings.Greeter;
 
 public class MainProgram {                                 // ez működik. automatikusan importálta a classt
     public static void main(String[] args) {
-        Greeter greeter = new Greeter();
-        System.out.println(greeter.sayHello());
+        // Greeter greeter = new Greeter();
+        // System.out.println(greeter.sayHello());
+
+        new Greeter().sayHello();                         // solution-ból a megoldás
     }
 }
