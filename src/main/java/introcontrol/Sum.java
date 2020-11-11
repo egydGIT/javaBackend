78fd6fd021253879d3f29a760a95dae3e1a7667a
Eszter <egyd.eszter@gmail.com>
2020-11-11 18:07:07 +0100

    Összegszámítás - ciklussal

--- src/main/java/introcontrol/Sum.java
+++ src/main/java/introcontrol/Sum.java
@@ -7,14 +7,22 @@ public class Sum {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Adj meg öt számot és összadom! ");
 
-        int number1 = scanner.nextInt();
+        /*
+        int number1 = scanner.nextInt();                                         // Egyszerűen
         int number2 = scanner.nextInt();
         int number3 = scanner.nextInt();
         int number4 = scanner.nextInt();
         int number5 = scanner.nextInt();
 
         System.out.println( number1 + number2 + number3 + number4 + number5 );
+        */
 
-
+        int sum = 0;                                                             // Ciklussal
+        for ( int i = 0; i < 5; i++ ) {
+            System.out.println("Add meg a " + (i + 1) + ". számot! ");
+            int number = scanner.nextInt();
+            sum += number;
+        }
+        System.out.println("Az összeg: " + sum);
     }
 }
