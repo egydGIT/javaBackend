6afec70c5cf07a16575cedf35e7e9410ad58d326
Eszter <egyd.eszter@gmail.com>
2020-11-12 12:11:30 +0100

    Tömbök kezelése - nem az az eredmény, amit várnék.

---
+++ src/main/java/array/ArrayMain.java
@@ -0,0 +1,64 @@
+package array;
+
+public class ArrayMain {
+
+    public static void main(String[] args) {
+        String [] daysOfWeek = {"Hétfő", "Kedd", "Szerda", "Csütörtök", "Péntek", "Szombat", "Vasárnap"};
+
+        System.out.println("Második nap: " + daysOfWeek[1]);
+        System.out.println("Napok száma: " + daysOfWeek.length);
+
+        /*
+        public class Main {                                 // Google hatvány
+        public static void main(String[] args) {
+            int alap = 2;
+            int kitevo = 4;
+            int eredmeny = 1;
+            for (int i = 1; i <= kitevo; i++){
+            eredmeny *= alap;
+            }
+        System.out.println(eredmeny);
+        }
+*/
+        int basic = 2;
+        int power = 5;
+        // int result = 1;
+        int [] result = new int[5];
+        result[0] = 1;
+        for ( int i = 0; i <= 4; i++) {
+            // result *= basic;
+            result[i] *= basic;
+            System.out.print(result[i] + " ");          // Konzol: 2 0 0 0 0          Miért?
+        }
+/*
+        System.out.println("");
+        for ( int i = 0; i < 5; i++) {
+            System.out.print(result[i] + " ");
+        }
+*/
+        System.out.println();
+        boolean [] b = new boolean[6];
+
+        for (int i = 1; i < b.length; i++) {
+            b[i] = !b[i-1];
+            System.out.print(b[i] + " ");
+        }                                               // true false true false true    Nem false kezdődik!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
