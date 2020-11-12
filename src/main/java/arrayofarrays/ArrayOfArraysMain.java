355c47bbe812b8f242c9627b772dfe1802ee9e98
Eszter <egyd.eszter@gmail.com>
2020-11-12 19:52:15 +0100

    Szorzótábla - version 3.0 - ez a feladat

--- src/main/java/arrayofarrays/ArrayOfArraysMain.java
+++ src/main/java/arrayofarrays/ArrayOfArraysMain.java
@@ -3,18 +3,39 @@ package arrayofarrays;
 import java.util.Scanner;
 
 public class ArrayOfArraysMain {
-/*
-    int [][] multiplicationTabla(int size) {
-        this.size = size;
+    public int [][] multiplicationTable(int size) {                              // version 3.  -  ez a feladat
+        int [][] multiplicationTable = new int[size+1][size+1];
+        for (int k = 1; k <= size; k++) {
+            for (int i = 1; i <= size; i++) {
+                multiplicationTable[k][i] = k * i;
+                if (k * i < 10 ){
+                    System.out.print((multiplicationTable[k][i] = k * i) + "  ");
+                } else if (k * i < 101) {
+                    System.out.print((multiplicationTable[k][i] = k * i) + " ");
+                }
+            }
+            System.out.println();
+        }
+        return multiplicationTable;
     }
-*/
-
 
 
 
     public static void main(String[] args) {
-        int [][] multiplicationTable = new int[11][12];
+        ArrayOfArraysMain arrayOfArraysMain = new ArrayOfArraysMain();
+
+        // Scanner scanner = new Scanner();
+        // System.out.println("Hányszor hányas szorzótábla?");
+        // int size = scanner.nextInt();
+
+        arrayOfArraysMain.multiplicationTable(4);
+        System.out.println();
+        arrayOfArraysMain.multiplicationTable(7);
+
+
 /*
+        int [][] multiplicationTable = new int[11][12];
+                                                                                // version 1.
         for (int k = 1; k <= 11; k++)  {                                        // Szorzótábla fejléccel ciklusból (önálló munka!!)
             System.out.println("A " + k + " szorzótábla: ");                    // Vmiért túlfut, azt még meg kell nézni.
             for ( int i = 1; i <= 10; i++) {
@@ -23,8 +44,8 @@ public class ArrayOfArraysMain {
             System.out.println();
             System.out.println();
         }
-*/
 
+                                                                                 // version 2.
         for (int k = 1; k <= 11; k++) {                                          // Szorzótábla négyzethálós ábrázolásban
             for (int i = 1; i <= 10; i++) {                                      // Vmi a végével ennek is van...
                 multiplicationTable[k][i] = k * i;
@@ -36,23 +57,8 @@ public class ArrayOfArraysMain {
             }
             System.out.println();
         }
-
-
-
-
-
-
-
-
-
-
-         /*
-        Scanner scanner = new Scanner();
-        System.out.println("Hanyas szorzótábla: ");
-        int k = scanner.nextInt();
-
-         */
-
+*/
+        
     }
 
 }
