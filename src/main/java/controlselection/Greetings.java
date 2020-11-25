4d465f5d2683af2f4b97448c1af3d519a2621329
Eszter <egyd.eszter@gmail.com>
2020-11-25 19:24:01 +0100

    Napszaktól függő köszönés - erre nagyon büszke vagyok!! :-)

---
+++ src/main/java/controlselection/Greetings.java
@@ -0,0 +1,55 @@
+package controlselection;
+
+import java.time.LocalTime;
+
+public class Greetings {
+
+    LocalTime now = LocalTime.now();
+
+    public int getHour(LocalTime now) {
+        int hour = now.getHour();
+        return hour;
+    }
+
+    public int getMinute(LocalTime now) {
+        int minute = now.getMinute();
+        return minute;
+    }
+
+    public int letsGreet(int hour, int minute) {
+        int allMinuteUntilNow = hour * 60 + minute;
+        int fiveClock = 300;
+        int nineClock = 540;
+        int halfPastSixPm = 1110;
+        int eightPm = 1200;
+
+
+        if (allMinuteUntilNow >= fiveClock && allMinuteUntilNow < nineClock ) {
+            System.out.println("Jó reggelt!");
+        }
+        if (allMinuteUntilNow >= nineClock && allMinuteUntilNow < halfPastSixPm ) {
+            System.out.println("Jó napot!");
+        }
+        if (allMinuteUntilNow >= halfPastSixPm && allMinuteUntilNow < eightPm ) {
+            System.out.println("Jó estét!");
+        }
+        if (allMinuteUntilNow >= eightPm && allMinuteUntilNow < fiveClock ) {
+            System.out.println("Jó éjt! Menj aludni! :-)  ");
+        }
+        return allMinuteUntilNow;
+    }
+
+
+
+    public static void main(String[] args) {
+        Greetings greetings = new Greetings();
+
+        System.out.println("Pontos idő: " + greetings.now);
+
+        // System.out.println(greetings.letsGreet(18, 51));
+
+        System.out.println("Óra: " + greetings.getHour(greetings.now));
+        System.out.println("Perc: " + greetings.getMinute(greetings.now));
+        System.out.println(greetings.letsGreet(greetings.getHour(greetings.now), greetings.getMinute(greetings.now)));
+    }
+}
