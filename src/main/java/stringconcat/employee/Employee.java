8d93ce34acf0a4ce57df67844e0fd390c814bb02
Eszter <egyd.eszter@gmail.com>
2020-11-29 22:33:53 +0100

    toString() + divisible with 1000

---
+++ src/main/java/stringconcat/employee/Employee.java
@@ -0,0 +1,45 @@
+package stringconcat.employee;
+
+public class Employee {
+    private String name;
+
+    private String profession;
+
+    private int salary;
+
+    public Employee(String name, String profession, int salary) {
+        if (isEmpty(name)) {
+            throw new IllegalArgumentException("Name must not be empty.");
+        }
+        if (isEmpty(profession)) {
+            throw new IllegalArgumentException("Job must not be empty.");
+        }
+        if (salary <= 0 && (salary % 1000 != 0)) {
+            throw new IllegalArgumentException("Salary must be positive and divisible with 1000.");
+        }
+        this.name = name;
+        this.profession = profession;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    @Override
+    public String toString() {
+        return name + " - " + profession + " - " + salary;
+    }
+
+    private boolean isEmpty(String str) {
+        return str == null || str.isEmpty();
+    }
+}
