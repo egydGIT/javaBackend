5b582d27ffc7d60753c8b4a5a3cc4c2076c2f422
Eszter <egyd.eszter@gmail.com>
2020-11-29 20:06:34 +0100

    String-ek (pool vs heap)

---
+++ src/main/java/stringbasic/PoolHeap.java
@@ -0,0 +1,18 @@
+package stringbasic;
+
+public class PoolHeap {
+
+    String s = "Sting literal with many characters.";
+
+    public String createStringForHeap(String s) {
+        this.s = s;
+        s = new String();                         // new String() mindenképpen új obj-ot hoz létre a heapen
+        return s;
+    }
+
+    public String createStringForPool(String s) {
+        this.s = s;
+        s = s.intern();                           // intern() vizsgálja, a String a Pool-ban van-e, ha nincs, beleteszi
+        return s;
+    }
+}
