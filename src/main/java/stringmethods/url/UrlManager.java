5f71088748ade06ed6fa76838b4fb1da4774057c
Eszter <egyd.eszter@gmail.com>
2020-11-30 02:24:58 +0100

    URL feldolgozás 1.0 - csak az UML diagramm

---
+++ src/main/java/stringmethods/url/UrlManager.java
@@ -0,0 +1,37 @@
+package stringmethods.url;
+
+public class UrlManager {
+    private String protocol;
+    private Integer port;
+    private String host;
+    private String path;
+    private String query;
+
+    public UrlManager(String url) {
+
+    }
+
+    public String getProtocol() {
+        return protocol;
+    }
+
+    public Integer getPort() {
+        return port;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public boolean hasProperty(String key) {
+        
+    }
+
+    public String getProperty(String key) {
+
+    }
+}
