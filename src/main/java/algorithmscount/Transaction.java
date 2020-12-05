dd3fe882d358d5a5aada34f8c4b6f9e50d61e1b8
EgydGIT <egyd.eszter@gmail.com>
2020-12-05 21:09:38 +0100

    Kis összegű tranzakciók

---
+++ src/main/java/algorithmscount/Transaction.java
@@ -0,0 +1,35 @@
+package algorithmscount;
+
+public class Transaction {
+    private String accountNumber;
+    private TransactionType transactionType;
+    private int amount;
+
+    public Transaction(String accountNumber, TransactionType transactionType, int amount) {
+        this.accountNumber = accountNumber;
+        this.transactionType = transactionType;
+        this.amount = amount;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+/*
+    public TransactionType getTransactionType() {
+        return transactionType;
+    }
+
+ */
+    public boolean isCredit() {
+        return transactionType == TransactionType.CREDIT;
+    }
+
+    public boolean isDebit() {
+        return transactionType == TransactionType.DEBIT;
+    }
+
+
+    public int getAmount() {
+        return amount;
+    }
+}
---
+++ src/main/java/algorithmscount/TransactionCounter.java
@@ -0,0 +1,29 @@
+package algorithmscount;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TransactionCounter {
+    public int TransactionsLowerThan (List<Transaction> transactions, int amount) {
+        int counter = 0;
+        for (Transaction transaction: transactions) {
+            if (transaction.getAmount() < amount) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    public static void main(String[] args) {
+        Transaction transaction1 = new Transaction("123", TransactionType.CREDIT, 8_500);
+        Transaction transaction2 = new Transaction("456", TransactionType.DEBIT, 7_500);
+        Transaction transaction3 = new Transaction("789", TransactionType.CREDIT, 9_780);
+
+        List<Transaction> transactions = new ArrayList<>();
+        transactions.add(transaction1);
+        transactions.add(transaction2);
+        transactions.add(transaction3);
+
+        System.out.println(new TransactionCounter().TransactionsLowerThan(transactions, 9_000));
+    }
+}
