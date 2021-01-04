e49c8d46c468e493b4afdba930deb1e2ccfcca3f
EgydGIT <egyd.eszter@gmail.com>
2021-01-04 13:47:14 +0100

    week04d03   +MyBonus

--- src/main/java/week04d03/GuessTheNumberMy.java
+++ src/main/java/week04d03/GuessTheNumberMy.java
@@ -36,8 +36,37 @@ public class GuessTheNumberMy {
         return number;
     }
 
+    public int guessTheNumberBonus() {
+        Random random = new Random();
+        int number = random.nextInt(100) +1;
+        // System.out.println(number);
+        System.out.println("Gondoltam egy számra 1 és 100 között. Találd ki! Csak 6 lehetőséged van!");
+        Scanner scanner = new Scanner(System.in);
+
+        int numberOfGuess = 0;
+        while (numberOfGuess < 6) {
+            System.out.println(numberOfGuess + 1 + ". lehetőség:");
+            int guessedNumber = scanner.nextInt();
+            if (guessedNumber > number) {
+                System.out.println("Ennél kisebbre gondoltam!");
+            }
+            if (guessedNumber < number) {
+                System.out.println("Ennél nagyobbra gondoltam!");
+            }
+            if (guessedNumber == number) {
+                System.out.println("Eltaláltad! A " + number + " számra gondoltam!");
+                break;
+            }
+            numberOfGuess++;
+            if (guessedNumber != number && numberOfGuess == 6)
+            System.out.println("Sajnos nem találtad el a 6. próbálkozásra sem!");
+        }
+        return number;
+    }
+
     public static void main(String[] args) {
         GuessTheNumberMy guess = new GuessTheNumberMy();
         guess.guessTheNumber();
+        guess.guessTheNumberBonus();
     }
 }
