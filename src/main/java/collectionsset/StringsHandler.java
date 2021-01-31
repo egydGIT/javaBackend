598bfd0c7ebe638fec2d968ef0fcd111c4971f29
EgydGIT <egyd.eszter@gmail.com>
2021-01-31 23:55:31 +0100

    collectionsset / StringsHandler

--- src/main/java/collectionsset/StringsHandler.java
+++ src/main/java/collectionsset/StringsHandler.java
@@ -6,17 +6,16 @@ import java.util.Set;
 
 public class StringsHandler {
 
-    public Set<String> filterUniqueStrings(Collection<String> stringCollection) {     // kiszűri az egyedi elemeket
-        Set<String> uniqueStrings = new HashSet<>(stringCollection);                  // nem enged duplikációt, equals met-t hívja meg
+    public Set<String> filterUniqueStrings(Collection<String> stringCollection) {  // kiszűri az egyedi elemeket
+        Set<String> uniqueStrings = new HashSet<>(stringCollection);               // Set-ben nem lehet duplikáció
         return uniqueStrings;
     }
 
-    public Set<String> selectIdenticalStrings(Set<String> setA, Set<String> setB) { // különböző String halmazból kinyeri a közös elemeket
+    public Set<String> selectIdenticalStrings(Set<String> setA, Set<String> setB) {  // két különböző String halmazból kinyeri a közös elemeket.
         setA.retainAll(setB);
-        //  If the specified collection is also a set, this operation effectively modifies this set so that
-        //  its value is the intersection of the two sets.
+        // If the specified collection is also a set, this operation effectively modifies this set
+        // so that its value is the intersection of the two sets.
         return setA;
     }
-
 
 }
