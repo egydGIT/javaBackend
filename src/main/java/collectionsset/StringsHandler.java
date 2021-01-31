e34e1426c19a3c1690b52a8263100490397d6e2b
EgydGIT <egyd.eszter@gmail.com>
2021-01-31 22:37:21 +0100

    collectionslist / Lottery, LotteryTest + Set

---
+++ src/main/java/collectionsset/StringsHandler.java
@@ -0,0 +1,22 @@
+package collectionsset;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+public class StringsHandler {
+
+    public Set<String> filterUniqueStrings(Collection<String> stringCollection) {     // kiszűri az egyedi elemeket
+        Set<String> uniqueStrings = new HashSet<>(stringCollection);                  // nem enged duplikációt, equals met-t hívja meg
+        return uniqueStrings;
+    }
+
+    public Set<String> selectIdenticalStrings(Set<String> setA, Set<String> setB) { // különböző String halmazból kinyeri a közös elemeket
+        setA.retainAll(setB);
+        //  If the specified collection is also a set, this operation effectively modifies this set so that
+        //  its value is the intersection of the two sets.
+        return setA;
+    }
+
+
+}
