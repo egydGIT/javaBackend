518b8aff577c8bb6a710c1a31d159f0d9d90bd77
EgydGIT <egyd.eszter@gmail.com>
2021-02-01 11:43:10 +0100

    collectionsmap / IntegerOperations

---
+++ src/main/java/collectionsmap/IntegerOperations.java
@@ -0,0 +1,41 @@
+package collectionsmap;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IntegerOperations {
+
+    private List<Integer> numbersList = new ArrayList<>();
+
+    public List<Integer> createList(int... numbers) {
+        for (int n: numbers) {
+            numbersList.add(n);
+        }
+        return numbersList;
+    }
+
+    public int sumIntegerList(List<Integer> integerList) {
+        int sum = 0;
+        for (int i: integerList) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    public int sumIntegerObjects(Integer... integers) {
+        int sum = 0;
+        for (Integer i: integers) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    @Override
+    public String toString() {
+        return "IntegerOperations{" +
+                "numbersList=" + numbersList +
+                '}';
+    }
+
+
+}
