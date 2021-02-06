d6ac54565c7bf1fbb8a9392c21fb55b83a0a5db7
EgydGIT <egyd.eszter@gmail.com>
2021-02-06 23:55:20 +0100

    searching / Book, BookArraySearch, BookArraySearchTest, books.csv - 1.1

--- src/main/java/searching/BookArraySearch.java
+++ src/main/java/searching/BookArraySearch.java
@@ -31,4 +31,19 @@ public class BookArraySearch {
     public void setBookArray(Book[] bookArray) {
         this.bookArray = bookArray;
     }
+
+
+    public static void main(String[] args) {
+
+        Book[] bookArray = {new Book("Title1", "Author1"),
+                            new Book("Title2", "Author2"),
+                            new Book("Title3", "Author3"),
+                            new Book("Title4", "Author4"),
+                            new Book("Title5", "Author5")};
+        BookArraySearch bas = new BookArraySearch(bookArray);
+        System.out.println(bas.findBookByAuthorTitle("Title2", "Author2"));
+        System.out.println(bas.findBookByAuthorTitle("", "Author8")); // Searching parameters are missing. Author:  or Title: Author8
+        System.out.println(bas.findBookByAuthorTitle("Title8", "Author8"));  // There's no matching book
+
+    }
 }
