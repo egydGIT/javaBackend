ae060dad4407cccc9089a4887a1ed9018c4100a8
EgydGIT <egyd.eszter@gmail.com>
2021-02-11 17:37:38 +0100

    week12d05 / CovidTest, testdata.txt - BufferedReader reader

--- src/main/java/week12d05/Covid.java
+++ src/main/java/week12d05/Covid.java
@@ -9,6 +9,7 @@ package week12d05;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -16,11 +17,15 @@ public class Covid {
 
     // index.hu -> Crtl U, Ctrl A, Ctrl C  -> new file: index.html  Ctrl V
 
-    public int wordCounter(String word, Path file) {
-        try (BufferedReader br = new BufferedReader(Files.newBufferedReader(file))) {
+    public int wordCounter(String word, BufferedReader reader) {    // fájlt vhonnan beolvasni: met. param: Reader, Writer, Input-, OutputStream
+                                                                    // BufferedReader tud sort olvasni
+                                                                    // Readert az hozza létre, aki maghívja a met-t
+                                                                            // -> main: Files.newBufferedReader((Path.of("")))  - forrás egy fájl
+                                                                            // -> test: new BufferedReader(new InputStreamReader(is))
+        try {
             int counter = 0;    // összegzés tételéhez változó: lokális vátozó, nincs kezdőértéke, inicializálni kell
             String line;
-            while ((line = br.readLine()) != null) {        // amíg talál beolvasható sort
+            while ((line = reader.readLine()) != null) {        // amíg talál beolvasható sort
                 // line változóba elmenti a beolvasott sort - ha az tartalmaz karaktereket
                 if (line.toLowerCase().contains(word)) {
                     // ha a beolvasott sor, kisbetűsítve, tartalmazza a met. par-ként átadott szót
@@ -35,13 +40,19 @@ public class Covid {
 
 
     public static void main(String[] args) {
-        Path file = Path.of("src/main/resources/index.html");       // korábban bent definiált vált-t par-be kivinni -> tesztelhetőbb a met.
+        // Path file = Path.of("src/main/resources/index.html");  // korábban bent definiált vált-t par-be kivinni -> tesztelhetőbb a met.
 
         Covid covid = new Covid();
-        System.out.println(covid.wordCounter("covid", file));     // 4
-        System.out.println(covid.wordCounter("vakcina", file));   // 6
-        System.out.println(covid.wordCounter("gazdaság", file));  // 3
-        System.out.println(covid.wordCounter("magyar", file));    // 29
+        BufferedReader reader = null;
+        try {
+            reader = Files.newBufferedReader(Path.of("src/main/resources/index.html"));
+            System.out.println(covid.wordCounter("covid", reader));     // 4
+            reader = Files.newBufferedReader(Path.of("src/main/resources/index.html"));  // ha végigolvassa, újra létre kell hozni, h az elejéről kezdje
+            System.out.println(covid.wordCounter("gazdaság", reader));     // 3
+        } catch (IOException e) {
+            throw new IllegalStateException("Cant...", e);
+        }
+
 
 
     }
--- src/test/java/week12d05/CovidTest.java
+++ src/test/java/week12d05/CovidTest.java
@@ -2,6 +2,8 @@ package week12d05;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.*;
+import java.net.URL;
 import java.nio.file.Path;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,17 +13,29 @@ class CovidTest {
     @Test
     void testWordCounter() {
         Covid c = new Covid();
-        Path file = Path.of("src/main/resources/index.html");
-        int counter = c.wordCounter("covid", file);
+        InputStream is = CovidTest.class.getResourceAsStream("/index.html");  // test esethez fájlt betölteni: getResourceAsStream
+                                                // fájl az src/main/resources könyvtárban: kell /     főkönyvtárban: nem kell /
+                                                // getResourceAsStream Steam-et ad vissza, met Path-t vár ->nem kompatibilis
+                                                // met par-t módosítani: Reader-t várjon
+        //Path file = Path.of();
+        int counter = c.wordCounter("covid", new BufferedReader(new InputStreamReader(is)));
         assertEquals(4, counter);
     }
 
     @Test
-    void testWordCounterWihtSimpleFile() {
+    void testWordCounterWithSimpleFile() {
         Covid c = new Covid();
-        Path file = Path.of("src/main/resources/testdata.txt");
-        int counter = c.wordCounter("semmi", file);
-        assertEquals(2, counter);
+        // Path file = Path.of("src/main/resources/testdata.txt");                 // fájl -> file
+        // InputStream is = CovidTest.class.getResourceAsStream("/testdata.txt");  // classpath-ról fájl -> new BufferedReader(new InputStreamReader(is))
+        String sample = "alma\n,körte\n,meggy\nalma";                              // mem-ból String -> new BufferedReader(new StringReader(sample))
+        try {
+            URL url = new URL("https://index.hu");                            // hálózatról -> new BufferedReader(new InputStreamReader(is))
+            InputStream is = url.openStream();
+            int counter = c.wordCounter("covid", new BufferedReader(new InputStreamReader(is)));
+            assertEquals(4, counter);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
