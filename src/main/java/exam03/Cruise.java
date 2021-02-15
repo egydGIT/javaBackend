20a4631e621584bb307872bdcd67e091cb4e7a14
EgydGIT <egyd.eszter@gmail.com>
2021-02-15 20:47:41 +0100

    exam03 - Cruise UML

--- src/main/java/exam03/Cruise.java
+++ src/main/java/exam03/Cruise.java
@@ -19,6 +19,68 @@ countPassengerByClass(): visszaadja, hogy osztályonként mennyien foglaltak
 
 package exam03;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class Cruise {
 
+    private Boat boat;
+    private LocalDate sailing;
+    private double basicPrice;
+
+    private List<Passenger> passengers = new ArrayList<>();
+
+    public Cruise(Boat boat, LocalDate sailing, double basicPrice) {
+        this.boat = boat;
+        this.sailing = sailing;
+        this.basicPrice = basicPrice;
+    }
+
+    public void bookPassenger(Passenger passenger) {
+        // egy foglalás. Ellenőrizni kell, hogy van-e még elég hely.
+
+    }
+
+    public double getPriceForPassenger(Passenger passenger) {
+        // visszaadja, hogy mennyibe kerülne a foglalás. Ez a metódus még NEM foglal.
+
+    }
+
+    public Passenger findPassengerByName(String name) {
+        // foglalás megkeresése név alapján
+
+    }
+
+    public List<String> getPassengerNamesOrdered() {
+        // visszaadja a foglalást végzők neveit ábécé sorrendben
+
+    }
+
+    public double sumAllBookingsCharged() {
+        // összegzi, hogy mennyi az összes bevétel
+
+    }
+
+    public Map<CruiseClass, Integer> countPassengerByClass() {
+        // visszaadja, hogy osztályonként mennyien foglaltak
+
+    }
+
+    public Boat getBoat() {
+        return boat;
+    }
+
+    public LocalDate getSailing() {
+        return sailing;
+    }
+
+    public double getBasicPrice() {
+        return basicPrice;
+    }
+
+    public List<Passenger> getPassengers() {
+        return passengers;
+    }
 }
