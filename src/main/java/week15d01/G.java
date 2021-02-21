121e5913a009024fbce33ad0fd7ef43d7e3c546e
EgydGIT <egyd.eszter@gmail.com>
2021-02-21 20:24:33 +0100

    week15d01 - Graph findEveryBiggest(), de nem ok

---
+++ src/main/java/week15d01/G.java
@@ -0,0 +1,65 @@
+/*
+Junior
+Adott egy függvénygrafikon a koordináta rendszerben. A grafikon néhány pontját megkapjuk egy Map-ben.
+A map kulcsa az x koordináta értéke pedig az y koordináta. Döntsük el, hogy a kapott pontok küzül,
+hol van a függvénynek maximum helye és ott mennyi az értéke.
+ */
+package week15d01;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class G {
+
+    public Map<Double, Double> maxEntry(Map<Double, Double> graphOfFunction) {  // Map-ben max értéket keres
+        if (graphOfFunction.size() == 0 ) {
+            throw new IllegalArgumentException("Wrong param.");
+        }
+        Map.Entry<Double, Double> foundResult = findFirstBiggest(graphOfFunction);
+        Map<Double, Double> foundResults = findEveryBiggest(foundResult, graphOfFunction);
+        return foundResults;
+    }
+
+    private Map<Double, Double> findEveryBiggest(Map.Entry<Double, Double> foundResult, Map<Double, Double> graphOfFunction) {
+        Map<Double, Double> results = new HashMap<>();
+        for ( Map.Entry<Double, Double> entry : graphOfFunction.entrySet()) {
+            if ( foundResult.getValue() == entry.getValue() ) {
+                results.put(entry.getKey(), entry.getValue());
+            }
+        }
+        return results;
+    }
+
+    private Map.Entry<Double, Double> findFirstBiggest(Map<Double, Double> graphOfFunction) {
+        Map.Entry<Double, Double> result = null;
+        // double max = Double.MIN_VALUE;
+        for ( Map.Entry<Double, Double> entry : graphOfFunction.entrySet() ) {
+            if (result == null || result.getValue() < entry.getValue()) {        // első legnagyobbat adja vissza
+                result = entry;
+                // max = entry.getValue();
+            }
+        }
+        return result;
+    }
+
+    // utólsó:  <=
+    // több max érték:   List<Map.Entry<Double, Double>>  ami tulképp: Map<Double, Double>
+    //  - két met: egyik megkeresi a max-ot
+    //  - következő az ezzel megegyező elemeket Map-be írja
+
+
+    public static void main(String[] args) {
+        Graph graph = new Graph();
+        Map<Double, Double> testGraph = new HashMap<>();
+        testGraph.put(-2.0, 9.0);
+        testGraph.put( -1.0, 2.0);
+        testGraph.put(0.0, -4.0);
+        testGraph.put(1.0, 9.0);
+        testGraph.put(2.0, 3.0);
+        testGraph.put(2.0, 9.0);
+
+        System.out.println(graph.maxEntry(testGraph));      // -2.0=9.0
+
+    }
+}
+
