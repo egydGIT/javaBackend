7fa79adb44bed67ef20ddf7d92c882dbe6c959ba
EgydGIT <egyd.eszter@gmail.com>
2021-03-14 14:00:03 +0100

    exam03 - Cruise, Histogram

--- src/main/java/exam03/Cruise.java
+++ src/main/java/exam03/Cruise.java
@@ -1,22 +1,3 @@
-/*
-Travel agency
-Utazási iroda hajóutakat szervez. Minden hajóút egy adott hajóval történik, amelynek neve és befogadóképessége a
-egy Boat osztályban van tárolva.
-
-Az utasok adott hajóra, adott osztályra foglalnak helyet. A helyfoglaláskor azonban ellenőrizni kell,
-hogy a befogadóképesség alapján van-e még hely. Ha nincs, egy kivételt várunk a megfelelő szöveggel.
-A CruiseClass enum tárolja a különböző osztályokat. LUXURY, FIRST és SECOND osztályok vannak.
-Az ár számolása úgy történik, hogy venni kell a hajóhoz tartozó alapárat,
-és meg kell szorozni az osztályhoz tartozó szorzóval, rendre 3,0, 1,8 és 1,0.
-
-bookPassenger(): egy foglalás. Ellenőrizni kell, hogy van-e még elég hely.
-getPriceForPassenger(): visszaadja, hogy mennyibe kerülne a foglalás. Ez a metódus még NEM foglal.
-findPassengerByName(): foglalás megkeresése név alapján
-getPassengerNamesOrdered(): visszaadja a foglalást végzők neveit ábécé sorrendben
-sumAllBookingsCharged(): összegzi, hogy mennyi az összes bevétel
-countPassengerByClass(): visszaadja, hogy osztályonként mennyien foglaltak
- */
-
 package exam03;
 
 import java.text.Collator;
--- src/main/java/exam03/Histogram.java
+++ src/main/java/exam03/Histogram.java
@@ -1,25 +1,3 @@
-/*
-Hisztogram
-Olvass be egy szöveges fájlt soronként, melyben soronkont számok vannak.
-Majd hozz létre egy stringet, mely ugyanennyi sort tartalmaz, és annyi csillagot ír ki egymás mellé soronkont,
-amennyi a bemeneti fájlban lévő szám.
-
-Azaz a bemeneti fájl:
-3
-4
-1
-2
-
-A létrehozott String:
-***
-****
-*
-**
-
-Figyelj, hogy az algoritmusod lehetőleg hatékony legyen!
-A Histogram osztályba dolgozz, tesztje HistogramTest. Minden sor után legyen sortörés, az utolsó után is.
- */
-
 package exam03;
 
 import java.io.BufferedReader;
