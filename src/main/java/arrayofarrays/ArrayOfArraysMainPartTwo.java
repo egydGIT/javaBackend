c327f19672bcca1f7b3cc5a8a34184a6772f08c0
EgydGIT <egyd.eszter@gmail.com>
2021-04-09 08:46:00 +0200

    arrayofarrays

--- src/main/java/arrayofarrays/ArrayOfArraysMainPartTwo.java
+++ src/main/java/arrayofarrays/ArrayOfArraysMainPartTwo.java
@@ -3,22 +3,52 @@ package arrayofarrays;
 import java.util.Arrays;
 
 public class ArrayOfArraysMainPartTwo {
-    /*
-    public int [][] printArrayOfArrays(int [][] a) {
 
+    public void printArrayOfArrays(int[][] a) {
+        for (int i[] : a) {
+            for (int j : i) {
+                if (j < 10) {
+                    System.out.print("  ");
+                } else if (j < 100) {
+                    System.out.print(" ");
+                }
+                System.out.print(j);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
     }
 
-    public int [][] triangularMatrix(int size) {                                                            // Ez solutionból, de nem értem.
-        int [][] triangularMatrix = new int[size][];
-        for ( int i = 0; i < triangularMatrix.length; i++) {
-            triangularMatrix[i] = new int[i+1];
+    public int[][] multiplicationTable(int size) {
+        int[][] multiplicationTable = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                multiplicationTable[i][j] = (i + 1) * (j + 1);
+            }
+        }
+        return multiplicationTable;
+    }
+
+    public int[][] triangularMatrix(int size) {
+        int[][] triangularMatrix = new int[size][];
+        for (int i = 0; i < triangularMatrix.length; i++) {
+            triangularMatrix[i] = new int[i + 1];
             for (int j = 0; j < i + 1; j++) {
                 triangularMatrix[i][j] = i;
             }
         }
         return triangularMatrix;
     }
-*/
+
+    public int[][] getValues() {
+        int[][] values = new int[12][];
+        int[] numberOfDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        for (int i = 0; i < numberOfDays.length; i++) {
+            values[i] = new int[numberOfDays[i]];
+        }
+        return values;
+    }
+
 
     public static void main(String[] args) {
         int [][] arraysInArray = { {1, 2, 3, 4}, {2, 2, 2, 2}, {0, 1, 0, 1, 0, 1, 0, 1}, {2, 4, 8, 16, 32, 64} };       // printArrayOfArrays version 1.0
@@ -30,8 +60,15 @@ public class ArrayOfArraysMainPartTwo {
 
         ArrayOfArraysMainPartTwo arrayOfArraysMainPartTwo = new ArrayOfArraysMainPartTwo();
 
-        // System.out.println(Arrays.deepToString(arrayOfArraysMainPartTwo.triangularMatrix(3)));
 
+        System.out.println("Multiplication table");
+        arrayOfArraysMainPartTwo.printArrayOfArrays(arrayOfArraysMainPartTwo.multiplicationTable(10));
+
+        System.out.println("Triangular matrix");
+        arrayOfArraysMainPartTwo.printArrayOfArrays(arrayOfArraysMainPartTwo.triangularMatrix(3));
+
+        System.out.println("Values per days");
+        arrayOfArraysMainPartTwo.printArrayOfArrays(arrayOfArraysMainPartTwo.getValues());
 
 
     }
