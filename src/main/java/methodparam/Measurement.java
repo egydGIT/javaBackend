7f1fd29989f52363acd675d6c083ace1d2c33fb6
EgydGIT <egyd.eszter@gmail.com>
2021-04-11 19:13:42 +0200

    methodparam

--- src/main/java/methodparam/Measurement.java
+++ src/main/java/methodparam/Measurement.java
@@ -9,16 +9,16 @@ public class Measurement {
         this.measurements = measurements;
     }
 
-    public int findFirstIndexInLimit(int lower, int upper) {   // Első olyan mérési eredmény indexe, ami a megadott határok között
+    public int findFirstIndexInLimitMy(int lower, int upper) {   // Első olyan mérési eredmény indexe, ami a megadott határok között
         for (int i = 0; i < measurements.length; i++) {                     // Eldöntés algoritmusa
-            if (measurements[i] > upper && measurements[i] < lower) {
+            if (measurements[i] < upper && measurements[i] > lower) {
                 return i;
             }
         }
         return -1;
     }
 
-    public int minimum() {                              // Legkisebb mérési eredmény: szélsőérték algoritmus
+    public int minimumMy() {                              // Legkisebb mérési eredmény: szélsőérték algoritmus
         int min = Integer.MAX_VALUE;
         for (Integer m: measurements) {
             if (m < min) {
@@ -28,7 +28,17 @@ public class Measurement {
         return  min;
     }
 
-    public int maximum() {                              // Legnagyobb mérési eredmény: szélsőérték algoritmus
+    public double minimum() {
+        double min = measurements[0];
+        for (int j = 1; j < measurements.length; j++) {
+            if (measurements[j] < min) {
+                min = measurements[j];
+            }
+        }
+        return min;
+    }
+
+    public int maximumMy() {                              // Legnagyobb mérési eredmény: szélsőérték algoritmus
         int max = Integer.MIN_VALUE;
         for (Integer m: measurements) {
             if (m > max) {
@@ -38,8 +48,17 @@ public class Measurement {
         return  max;
     }
 
-    public ExtremeValues minMax() {                     // Legnagyobb és legkisebb mérési eredményt adja vissza
-        return new ExtremeValues(minimum(), maximum());         // ehhez új osztály (immutable tipusú), ezt példányosítja
+    public double maximum() {
+        double max = measurements[0];
+        for (int j = 1; j < measurements.length; j++) {
+            if (measurements[j] > max)
+                max = measurements[j];
+        }
+        return max;
+    }
+
+    public ExtremeValues minMaxMy() {                     // Legnagyobb és legkisebb mérési eredményt adja vissza
+        return new ExtremeValues(minimumMy(), maximumMy());         // ehhez új osztály (immutable tipusú), ezt példányosítja
     }                                                           // melynek értékeit a fenti szélsőérték metódusok szolgáltatják
 
 
@@ -47,10 +66,10 @@ public class Measurement {
         int [] measurements = {25, 58, 69, 42, 71, 65, 66, 32, 3, 75, 97};
         System.out.println("Mérési eredmények: " + Arrays.toString(measurements));
         Measurement measurement = new Measurement(measurements);
-        System.out.println("Min: " + measurement.minimum());
-        System.out.println("Max: " + measurement.maximum());
+        System.out.println("Min: " + measurement.minimumMy());
+        System.out.println("Max: " + measurement.maximumMy());
 
-        ExtremeValues extremeValues = new ExtremeValues(measurement.minimum(), measurement.maximum());
+        ExtremeValues extremeValues = new ExtremeValues(measurement.minimumMy(), measurement.maximumMy());
         System.out.println("ExtremeValues: " + extremeValues.toString());
 
     }
