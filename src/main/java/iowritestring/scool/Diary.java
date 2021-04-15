cb4f5cfa6e035ecc302eb0b099b74f3920c611fa
EgydGIT <egyd.eszter@gmail.com>
2021-04-15 22:32:25 +0200

    iowritestring  -  !

--- src/main/java/iowritestring/scool/Diary.java
+++ src/main/java/iowritestring/scool/Diary.java
@@ -8,48 +8,76 @@ import java.util.List;
 
 public class Diary {
 
-    public void newMark(Path file,int mark) {
+//    public void newMarkMy(String studentName,int mark) {
+//        Path file = Path.of(studentName + ".txt");
+//        try {
+//            if (Files.exists(file)) {
+//                Files.writeString(file, mark + "\n", StandardOpenOption.APPEND);
+//            }
+//            else  {
+//                Files.writeString(file, mark + "\n");
+//            }
+//        } catch (IOException e) {
+//            throw new IllegalArgumentException("Cant find file", e);
+//        }
+//    }
+
+    public void newMark(Path file, int mark) {
         try {
             if (Files.exists(file)) {
                 Files.writeString(file, mark + "\n", StandardOpenOption.APPEND);
-            }
-            if (!Files.exists(file)) {
-                Files.writeString(file, mark + "\n");
+            } else {
+                Files.writeString(file, Integer.toString(mark));
             }
         } catch (IOException e) {
-            throw new IllegalArgumentException("Cant find file", e);
+            throw new IllegalStateException("Can't open file", e);
         }
     }
 
+//    public void averageMy(Path file) {
+//        try {
+//            List<String> marks = Files.readAllLines(file);
+//            double sum = 0.0;
+//            for (String m: marks) {
+//                if (!m.substring(0, 1).equals("A")) {
+//                    sum += Double.parseDouble(m);
+//                }
+//            }
+//            // String sum2 = String.format("Average: %.2" + sum);
+//            Files.writeString(file, "Average: " + (Math.round(sum) / marks.size()) + "\n", StandardOpenOption.APPEND);
+//
+//        } catch (IOException e) {
+//            throw new IllegalArgumentException("Cant find file", e);
+//        }
+//    }
+
     public void average(Path file) {
         try {
-            List<String> marks = Files.readAllLines(file);
-            double sum = 0.0;
-            for (String m: marks) {
-                if (!m.substring(0, 1).equals("A")) {
-                    sum += Double.parseDouble(m);
-                }
+            List<String> myFile = Files.readAllLines(file);
+            double sum = 0;
+            for (String s : myFile) {
+                sum += Double.parseDouble(s);
             }
 
-            Files.writeString(file, "Average: "+ Math.round(sum) / marks.size() + "\n", StandardOpenOption.APPEND);
+            Files.writeString(file, "Average: " + (sum / myFile.size()), StandardOpenOption.APPEND);
 
         } catch (IOException e) {
-            throw new IllegalArgumentException("Cant find file", e);
+            throw new IllegalStateException("Can't open file", e);
         }
     }
 
     public static void main(String[] args) {
-        Diary diary = new Diary();
-
-        diary.newMark(Path.of("src/main/resources/tanulo_neve.txt"), 5);
-        diary.newMark(Path.of("src/main/resources/tanulo_neve.txt"), 3);
-
-        diary.average(Path.of("src/main/resources/tanulo_neve.txt"));
-
-        diary.newMark(Path.of("src/main/resources/tanulo_neve.txt"), 1);
-        diary.newMark(Path.of("src/main/resources/tanulo_neve.txt"), 5);
-
-        diary.average(Path.of("src/main/resources/tanulo_neve.txt"));
+//        Diary diary = new Diary();
+//
+//        diary.newMarkMy("tanulo_neve", 5);
+//        diary.newMarkMy("tanulo_neve", 3);
+//
+//        diary.averageMy(Path.of("tanulo_neve.txt"));
+//
+//        diary.newMarkMy("tanulo_neve", 1);
+//        diary.newMarkMy("tanulo_neve", 5);
+//
+//        diary.averageMy(Path.of("tanulo_neve.txt"));
 
     }
 
--- src/test/java/iowritestring/scool/DiaryTest.java
+++ src/test/java/iowritestring/scool/DiaryTest.java
@@ -13,34 +13,30 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class DiaryTest {
 
-    @Test
-    void testNewMark() {
-        Diary diary = new Diary();
-
-        diary.newMark(Path.of("src/main/resources/tanulo_neve2.txt"), 5);
-        diary.newMark(Path.of("src/main/resources/tanulo_neve2.txt"), 3);
-
-        List<String> testList = null;
-        try {
-            testList = Files.readAllLines(Path.of("src/main/resources/tanulo_neve2.txt"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        assertEquals(2, testList.size());
-        assertEquals("3", testList.get(1));
-
-        diary.average(Path.of("src/main/resources/tanulo_neve2.txt"));
-
-        //assertEquals("4", testList.get(2).substring(8));
-
-    }
-
-/*
     @TempDir
     public Path temporaryFolder;
 
     private Diary diary = new Diary();
+
+//    @Test
+//    void testNewMarkMy() throws IOException {
+//
+//        diary.newMarkMy("tanulo_neve", 5);
+//        diary.newMarkMy("tanulo_neve", 3);
+//
+//        List<String> testList = null;
+//        try {
+//            testList = Files.readAllLines(Path.of("tanulo_neve.txt"));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        assertEquals(2, testList.size());
+//        assertEquals("3", testList.get(1));
+//
+//        diary.average(Path.of("tanulo_neve.txt"));
+//
+//    }
 
     @Test
     public void newMarkTestIfNotExists() throws IOException {
@@ -76,9 +72,9 @@ class DiaryTest {
 
         List<String> avgTestList = Files.readAllLines(file);
         assertEquals(4, avgTestList.size());
-        assertEquals("average: 4.0", avgTestList.get(avgTestList.size() - 1));
+        assertEquals("Average: 4.0", avgTestList.get(avgTestList.size() - 1));
 
     }
 
- */
+
 }
