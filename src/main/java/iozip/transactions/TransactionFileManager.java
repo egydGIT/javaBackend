2174c9bbae47cb203b8b574405b66a3d28780357
EgydGIT <egyd.eszter@gmail.com>
2021-04-19 20:36:33 +0200

    iozip.transactions

---
+++ src/main/java/iozip/transactions/TransactionFileManager.java
@@ -0,0 +1,38 @@
+package iozip.transactions;
+
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class TransactionFileManager {
+
+    private List<Transaction> transactions = new ArrayList<>();
+
+    public void saveTransactions(Path file, List<Transaction> transactions) {
+        try (ZipOutputStream zos = new ZipOutputStream(new BufferedOutputStream(Files.newOutputStream(file)))) {
+            for (Transaction t: transactions) {
+                String actualFilename = Long.toString(t.getId());
+                zos.putNextEntry(new ZipEntry(actualFilename));
+//                DataOutputStream dos = new DataOutputStream(zos);
+//                dos.writeUTF(t.getTime().toString() + "\n");
+//                dos.writeUTF(t.getAccount() + "\n");
+//                dos.writeDouble(t.getAmount());
+                String content = t.getTime().toString() + "\n" + t.getAccount() + "\n" + Double.toString(t.getAmount());
+                zos.write(content.getBytes());
+            }
+        } catch (IOException e) {
+            throw new IllegalStateException("Can not write file", e);
+        }
+    }
+
+    public List<Transaction> getTransactions() {
+        return new ArrayList<>(transactions);
+    }
+}
