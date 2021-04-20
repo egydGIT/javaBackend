09fbb9ff831486815ad977929adeb1eb48884989
EgydGIT <egyd.eszter@gmail.com>
2021-04-20 22:25:38 +0200

    collectionsset - random karaktersor generálás

--- src/test/java/collectionsset/RandomStringGenerator.java
+++ src/test/java/collectionsset/RandomStringGenerator.java
@@ -14,14 +14,14 @@ public class RandomStringGenerator {
     }
 
     private String randomString(int length) {
-        StringBuilder sb = new StringBuilder("");
-        Random rnd = new Random();
+        StringBuilder sb = new StringBuilder("");     // StringBuilder, mert mutable
+        Random rnd = new Random();                    // pseudorandom számot generál
         for (int i = 0; i < length; i++) {
-            int code = rnd.nextInt('z' - 'a');
-            code += 'a';
-            char x = (char) code;
-            sb.append(x);
+            int code = rnd.nextInt('z' - 'a');  // 122 - 97 = 25 -> 0 és 25 között generál egy véletlen számot
+            code += 'a';                              // hozzáadja 'a' karakterkódját -> 97 és 122 között ad egy számot
+            char x = (char) code;                     // ezt a számot explicit konvertálja karakterré
+            sb.append(x);                             // SringBuilder metódus, összefűzi a véletlen karakter sorozatot
         }
-        return sb.toString();
+        return sb.toString();                         // SB-t Stringgé konvertálja
     }
 }
